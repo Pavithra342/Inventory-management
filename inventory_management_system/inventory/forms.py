fa283839cfd9cd54d1b5b6a416a3a227cbf871c6
Pavithra <pavithra@transportsimple.com>
2025-05-28 18:26:24 +0530

    message

--- inventory_management_system/inventory/forms.py
+++ inventory_management_system/inventory/forms.py
@@ -17,3 +17,7 @@ class MobileForm(forms.ModelForm):
     class Meta:
         model = Mobiles
         fields = ('type', 'price', 'status', 'issues')
+class Mobile(forms.ModelForm):
+    class meta:
+        model = phones
+        fields = ('type', 'price', 'status', 'issues')
